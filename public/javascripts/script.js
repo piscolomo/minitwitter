a80723a824751536d7d9de2fbc69c34b98b3b6a9
Julio <ljuliom@gmail.com>
2014-05-21 20:51:56 -0500

    publish tweet

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -4,3 +4,15 @@ socket.on('login', function(data){
 	var name = prompt("Cual es tu nombre?");
 	document.getElementById("name").innerHTML = name;
 });
+
+window.onload = function(){
+	var tweetbutton = document.getElementById("tweetbutton");
+	var tweetmessage = document.getElementById("tweetmessage");
+	window.sectionright = document.getElementById("right");
+	tweetbutton.addEventListener("click", function(e){
+		e.preventDefault();
+		var newtweet = document.createElement("div");
+		newtweet.innerHTML = '<h3>' + document.getElementById("name").innerHTML + '</h3><p>' + tweetmessage.value + '</p>';
+		document.getElementById("right").appendChild(newtweet);
+	});
+};
